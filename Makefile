0e75b30aff9148343cde984224435346731c847e
Anthony <anton.ad99@gmail.com>
2020-03-03 22:23:57 -0500

    Hello there

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ all: prodcon
 
 clean:
 	@rm -rf prodcon 
+	@rm -rf *.log
 
-prodcon: prodcon.cpp 
-	g++ -std=c++11 prodcon.cpp -o prodcon -lpthread
+tands.o: tands.h tands.c
+		gcc -c tands.c tands.h
+
+prodcon: prodcon.cpp tands.o
+	g++ -std=c++11 prodcon.cpp tands.o -o prodcon -lstdc++ -lpthread
--- prodcon.cpp
+++ prodcon.cpp
@@ -13,7 +13,8 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
-
+#include "tands.h"
+#include <chrono>
 using namespace std;
 const int LINE_LENGTH = 100; // Max # of charactersinan input line
 const int MAX_ARGS = 7; // Max number of arguments to a command
@@ -32,15 +33,26 @@ char* args[MAX_ARGS + 1];
 // #define MAX_INPUT_LINE_LENGTH 10	// Max length of work line in input (e.g. T5 or S9)
 // #define MAX_OUTPUT_LINE_LENGTH 50	// Max length of output line, chosen as 50 for safe number
 condition_variable cv;
-mutex lockMutex;
-int numThreads, threadId;
+mutex workMutex, writeMutex;
+int numThreads, threadId, workCount, sleepCount, receiveCount, askCount, completeCount;
+FILE* output;
 void threadJob(int threadId);
 void getInput();
 void pushToQueue(int workAmount);
 int popFromQueue();
-
+void printToFile(int id, int work, string operation, bool queue_size);
+void closeFile();
+void initFile(int threadId);
+double getCurrRealTime();
+chrono::nanoseconds start;
+void printSummary();
 int main(int argc, char *argv[]) {
 
+	workCount = 0;
+	sleepCount = 0;
+	receiveCount = 0;
+	askCount = 0;
+	completeCount = 0;
 	if (argc == 1) {
 		// no arguement, error
 		perror("No argument passed");
@@ -53,7 +65,10 @@ int main(int argc, char *argv[]) {
 		numThreads = atoi(argv[1]);
 		threadId = atoi(argv[2]);
 	}
+	initFile(threadId);
 
+	//Start timer
+	// start = std::chrono::duration_cast<std::chrono::nanoseconds>(chrono::steady_clock::now()).count();
 
 	thread consumers[numThreads];
 
@@ -64,9 +79,12 @@ int main(int argc, char *argv[]) {
 
 
 	for ( int i = 0;  i < numThreads; i++) {
-		printf("Joining thread: %d\n", i);
+
 		consumers[i].join();
+		printf("Joining thread: %d\n", i);
 	}
+	printSummary();
+	closeFile();
 	return 0;
 }
 
@@ -74,9 +92,19 @@ int main(int argc, char *argv[]) {
 
 void threadJob(int threadId) {
 	while (1) {
-		printf("Thread %d asking for work\n",threadId);
+		askCount++;
+		printToFile(threadId, NULL , "Ask", false);
 		int workAmount = popFromQueue();
-		printf("Got work:%d\n", workAmount);
+
+
+		if (workAmount == -1) {
+			break;
+		}
+		receiveCount++;
+		printToFile(threadId, workAmount , "Receive", true);
+		Trans(workAmount);
+		completeCount++;
+		printToFile(threadId, workAmount , "Complete", false);
 	}
 
 }
@@ -94,23 +122,30 @@ void getInput() {
 			int workAmount = atoi(inputLine.substr(1).c_str());
 			//Transaction
 			pushToQueue(workAmount);
+			workCount++;
+			printToFile(0, workAmount , "Work", true);
 			//add to buffer queue
 		} else if (strcmp(operation, "S") == 0) {
 			// Producer sleeps Sleep
 			// add to buffer queue
+			sleepCount++;
 			length = atoi(inputLine.substr(1).c_str());
+			printf("%s\n", "Main process is going to sleep" );
+			printToFile(0, length , "Sleep", false);
+			Sleep(length);
 
 		}
 
 
 
 	}
-	printf("%s\n", "Done reading Input");
+	printToFile(0, NULL , "End", false);
+	pushToQueue(-1);
 }
 void pushToQueue(int workAmount) {
-	unique_lock<mutex> lk(lockMutex);
-	cv.wait(lk, []() {return (workQueue.size() != numThreads * 2);});
-
+	unique_lock<mutex> lk(workMutex);
+	cv.wait(lk, []() {return !(workQueue.size() >= numThreads * 2);});
+	printf("Work Queue size:%lu\n", workQueue.size() );
 	workQueue.push(workAmount);
 
 	lk.unlock();
@@ -118,15 +153,71 @@ void pushToQueue(int workAmount) {
 }
 
 int popFromQueue() {
-	unique_lock<mutex> lk(lockMutex);
+	unique_lock<mutex> lk(workMutex);
 	cv.wait(lk, []() {return (workQueue.size() != 0);});
 
 	int workAmount = workQueue.front();
-	workQueue.pop();
+	if (workAmount != -1) {
+		workQueue.pop();
+	}
+
 
 	lk.unlock();
 	cv.notify_all();
 	return workAmount;
 }
+void initFile(int id) {
+	string outputFileName;
+	if (threadId == 0) {
+		outputFileName = "prodcon.log";
+	} else {
+		outputFileName = "prodcon." + to_string(id) ;
+		outputFileName += ".log";
+
+	}
+	output = fopen(outputFileName.c_str(), "w");
+
+}
+void closeFile() {
+	fclose(output);
+}
+void printToFile(int id, int work, string operation, bool queue_size) {
+	char buffer [100];
+	unique_lock<mutex> lk(writeMutex);
+	if (work == NULL) {
+		sprintf (buffer, "%.3f ID= %d      %s\n", getCurrRealTime(), id, operation.c_str());
+		fputs(buffer, output);
+	} else if (queue_size) {
+		sprintf (buffer, "%.3f ID= %d Q= %ld %s %4d\n", getCurrRealTime(), id, workQueue.size(), operation.c_str(), work);
+		fputs(buffer, output);
+	} else {
+		sprintf (buffer, "%.3f ID= %d      %s %4d\n", getCurrRealTime(), id, operation.c_str(), work);
+		fputs(buffer, output);
+	}
+
+	lk.unlock();
+}
+double getCurrRealTime() {
+	// auto now = chrono::steady_clock::now();
+	// chrono::nanoseconds end = std::chrono::duration<double, nano>(now).count();
+	// chrono::nanoseconds curr = end - start;
+	// return chrono::duration <double> (curr).count();
+	return 0.00;
+}
+void printSummary() {
+	char buffer [100];
+	sprintf (buffer, "Summary:\n", workCount);
+	fputs(buffer, output);
+	sprintf (buffer, "	Work %d\n", workCount);
+	fputs(buffer, output);
+	sprintf (buffer, "	Ask %d\n", askCount );
+	fputs(buffer, output);
+	sprintf (buffer, "	Receive %d\n", receiveCount);
+	fputs(buffer, output);
+	sprintf (buffer, "	Complete %d\n", completeCount);
+	fputs(buffer, output);
+	sprintf (buffer, "	Sleep %d\n", sleepCount);
+	fputs(buffer, output);
 
 
+}
