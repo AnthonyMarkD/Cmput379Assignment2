7be2bb5fb719da6d36a7c1ca8fd26e4ce7a608bf
Anthony <anton.ad99@gmail.com>
2020-03-03 23:03:20 -0500

    Done

--- prodcon.cpp
+++ prodcon.cpp
@@ -28,6 +28,7 @@ struct timeval user_time, system_time;
 
 char input[100];
 char* args[MAX_ARGS + 1];
+chrono::time_point<std::chrono::system_clock> start;
 
 
 // #define MAX_INPUT_LINE_LENGTH 10	// Max length of work line in input (e.g. T5 or S9)
@@ -44,7 +45,6 @@ void printToFile(int id, int work, string operation, bool queue_size);
 void closeFile();
 void initFile(int threadId);
 double getCurrRealTime();
-chrono::nanoseconds start;
 void printSummary();
 int main(int argc, char *argv[]) {
 
@@ -61,34 +61,34 @@ int main(int argc, char *argv[]) {
 		numThreads = atoi(argv[1]);
 		threadId = 0;
 	} else {
-		//
+		// set values to variables
 		numThreads = atoi(argv[1]);
 		threadId = atoi(argv[2]);
 	}
 	initFile(threadId);
+	start = std::chrono::system_clock::now();
 
-	//Start timer
-	// start = std::chrono::duration_cast<std::chrono::nanoseconds>(chrono::steady_clock::now()).count();
-
+	// Start consumer threads
 	thread consumers[numThreads];
 
 	for ( int i = 0;  i < numThreads; i++) {
 		consumers[i] = thread(threadJob, i + 1);
 	}
 	getInput();
-
-
+	// end consumer threads by joining them.
 	for ( int i = 0;  i < numThreads; i++) {
 
 		consumers[i].join();
-		printf("Joining thread: %d\n", i);
+		
 	}
+	//print summary
 	printSummary();
+	// close output file like good smart people
 	closeFile();
 	return 0;
 }
 
-
+// What our threads do, simply grab stuff from the queue and print to a file.
 
 void threadJob(int threadId) {
 	while (1) {
@@ -116,7 +116,7 @@ void getInput() {
 		// get input from file or cmd line
 
 		string inputLine = input;
-		printf("Reading input:%s\n", input);
+		
 		const char* operation = inputLine.substr(0, 1).c_str();
 		if (strcmp(operation, "T") == 0) {
 			int workAmount = atoi(inputLine.substr(1).c_str());
@@ -130,7 +130,7 @@ void getInput() {
 			// add to buffer queue
 			sleepCount++;
 			length = atoi(inputLine.substr(1).c_str());
-			printf("%s\n", "Main process is going to sleep" );
+			
 			printToFile(0, length , "Sleep", false);
 			Sleep(length);
 
@@ -140,18 +140,20 @@ void getInput() {
 
 	}
 	printToFile(0, NULL , "End", false);
+	// push to queue variable which causes threads to finish and allows them to join
 	pushToQueue(-1);
 }
+// pushes work to queue, queue is protected buy a unique lock mutex which waits for the queue to not be full
 void pushToQueue(int workAmount) {
 	unique_lock<mutex> lk(workMutex);
 	cv.wait(lk, []() {return !(workQueue.size() >= numThreads * 2);});
-	printf("Work Queue size:%lu\n", workQueue.size() );
+	
 	workQueue.push(workAmount);
 
 	lk.unlock();
 	cv.notify_all();
 }
-
+// pushes work to queue, queue is protected buy a unique lock mutex which waits for the queue to not be empty
 int popFromQueue() {
 	unique_lock<mutex> lk(workMutex);
 	cv.wait(lk, []() {return (workQueue.size() != 0);});
@@ -166,6 +168,7 @@ int popFromQueue() {
 	cv.notify_all();
 	return workAmount;
 }
+// initializes our output file
 void initFile(int id) {
 	string outputFileName;
 	if (threadId == 0) {
@@ -178,45 +181,51 @@ void initFile(int id) {
 	output = fopen(outputFileName.c_str(), "w");
 
 }
+//closes the output file
 void closeFile() {
 	fclose(output);
 }
+// print to output file, use a mutex to prevent garbled garbage from occuring.
 void printToFile(int id, int work, string operation, bool queue_size) {
 	char buffer [100];
 	unique_lock<mutex> lk(writeMutex);
 	if (work == NULL) {
-		sprintf (buffer, "%.3f ID= %d      %s\n", getCurrRealTime(), id, operation.c_str());
+		sprintf (buffer, "%.3f ID= %d      %8s\n", getCurrRealTime(), id, operation.c_str());
 		fputs(buffer, output);
 	} else if (queue_size) {
-		sprintf (buffer, "%.3f ID= %d Q= %ld %s %4d\n", getCurrRealTime(), id, workQueue.size(), operation.c_str(), work);
+		sprintf (buffer, "%.3f ID= %d Q= %ld %8s %5d\n", getCurrRealTime(), id, workQueue.size(), operation.c_str(), work);
 		fputs(buffer, output);
 	} else {
-		sprintf (buffer, "%.3f ID= %d      %s %4d\n", getCurrRealTime(), id, operation.c_str(), work);
+		sprintf (buffer, "%.3f ID= %d      %8s %5d\n", getCurrRealTime(), id, operation.c_str(), work);
 		fputs(buffer, output);
 	}
 
 	lk.unlock();
 }
+//Gets the current time compared to start of the program.
 double getCurrRealTime() {
-	// auto now = chrono::steady_clock::now();
-	// chrono::nanoseconds end = std::chrono::duration<double, nano>(now).count();
-	// chrono::nanoseconds curr = end - start;
-	// return chrono::duration <double> (curr).count();
-	return 0.00;
+
+	chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+
+	double difference = chrono::duration<double>(now - start).count();
+	return difference;
+
 }
 void printSummary() {
 	char buffer [100];
-	sprintf (buffer, "Summary:\n", workCount);
+	sprintf (buffer, "Summary:\n");
 	fputs(buffer, output);
-	sprintf (buffer, "	Work %d\n", workCount);
+	sprintf (buffer, "	Work     %d\n", workCount);
 	fputs(buffer, output);
-	sprintf (buffer, "	Ask %d\n", askCount );
+	sprintf (buffer, "	Ask      %d\n", askCount );
 	fputs(buffer, output);
-	sprintf (buffer, "	Receive %d\n", receiveCount);
+	sprintf (buffer, "	Receive  %d\n", receiveCount);
 	fputs(buffer, output);
 	sprintf (buffer, "	Complete %d\n", completeCount);
 	fputs(buffer, output);
-	sprintf (buffer, "	Sleep %d\n", sleepCount);
+	sprintf (buffer, "	Sleep    %d\n", sleepCount);
+	fputs(buffer, output);
+	sprintf (buffer, "Transactions per second: %.2f\n", double(workCount) / getCurrRealTime());
 	fputs(buffer, output);
 
 
